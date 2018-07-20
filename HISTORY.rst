03652df72593484f704949c34935548819f6732d
LiyuanLucasLiu <llychinalz@gmail.com>
2018-07-19 17:10:12 -0700

    first commit

---
+++ HISTORY.rst
@@ -0,0 +1,7 @@
+History
+=======
+0.1 (2018-07-19)
+------------------
+* First commit. Reference:
+https://github.com/lanpa/tensorboardX
+https://github.com/allenai/allennlp
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'numpy',
+    'protobuf >= 0.3.2',
+    'six',
+    'tensorboardX'
+]
+
+setup(
+    name='tensorboard-wrapper',
+    version='0.1',
+    description='wrapper of TensorBoardX for pytorch models',
+    long_description= history,
+    author='Lucas Liu',
+    author_email='llychinalz@gmail.com',
+    url='https://github.com/LiyuanLucasLiu/tensorboard-wrapper',
+    packages=find_packages(exclude=['docs']),
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT license',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
+# python setup.py sdist bdist_wheel --universal
+# twine upload dist/*

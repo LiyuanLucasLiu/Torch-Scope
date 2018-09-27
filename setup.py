c0a7c8a001cfb496788d12dac4b8759bd2934b84
LiyuanLucasLiu <llychinalz@gmail.com>
2018-09-27 18:56:44 -0500

    debugged for 0.3.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name='torch-scope',
-    version='0.3.0',
+    version='0.3.1',
     description='A Toolkit for Training, Tracking and Saving PyTorch Models',
     long_description= history,
     author='Lucas Liu',
